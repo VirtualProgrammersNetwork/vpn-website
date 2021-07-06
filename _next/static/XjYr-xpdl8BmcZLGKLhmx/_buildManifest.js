2f31dbb595dcfe87f6f6642a63d4d917ec5ea015
kurone-kito <kurone-kito@users.noreply.github.com>
2021-07-06 13:05:17 +0000

    deploy: a95c4f80cb0a0f40a0f3763a898da55c83a26f93

---
+++ _next/static/XjYr-xpdl8BmcZLGKLhmx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8a16d34adde60d7b35a4.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
