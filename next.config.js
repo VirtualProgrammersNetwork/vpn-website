9254757ecd3ba0d8c98b65f682b8f0d7e5354973
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-08-28 18:24:41 +0900

    build: install packages and write transpile script for markdown

--- next.config.js
+++ next.config.js
@@ -3,3 +3,24 @@ const providedExports = { future: { strictPostcssConfiguration: true } };
 
 /** @type {Partial<import('next/dist/next-server/server/config-shared').NextConfig>} */
 module.exports = providedExports;
+
+// Next.jsで ES module を使うために必要な処理, issue #6 参照
+const withTM = require('next-transpile-modules')([
+  'react-markdown',
+  'micromark-util-combine-extensions',
+  'micromark-util-symbol',
+  'micromark-util-resolve-all',
+  'micromark-util-html-tag-name',
+  'remark-rehype',
+  'mdast-util-to-hast',
+  'unist-builder',
+  'unist-util-position',
+  'unist-util-generated',
+  'mdast-util-definitions',
+  'property-information',
+  'unist-util-visit',
+  'space-separated-tokens',
+  'comma-separated-tokens',
+]);
+
+module.exports = withTM(providedExports);
