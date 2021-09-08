d60a026795128a8fa2473653feec2ccfd99d3af9
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-09-08 16:22:36 +0900

    feat: simple layout component includes seo head, header, and footer

---
+++ src/components/layout/header-nav.tsx
@@ -0,0 +1,5 @@
+const HeaderNav = (): JSX.Element => (
+  <nav className="flex flex-row justify-end" />
+);
+
+export default HeaderNav;
---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,23 @@
+import { NextSeo } from 'next-seo';
+import { ReactNode } from 'react';
+// eslint-disable-next-line sort-imports
+import Footer from './footer';
+import Header from './header';
+
+type Props = {
+  title: string;
+  children: ReactNode;
+};
+
+const Layout: React.FC<Props> = ({ title, children }) => (
+  <>
+    <NextSeo title={title} />
+    <Header />
+    <main className="max-w-screen-2xl container mx-auto my-2" tw="prose">
+      {children}
+    </main>
+    <Footer />
+  </>
+);
+
+export default Layout;
