4fd3ebbbb77d62ff39afe4ab5635605775edc641
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-09-08 16:19:18 +0900

    feat: set default seo

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+  titleTemplate: '%s | VPN',
+  defaultTitle: 'VPN Official Web',
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    site_name: "Virtual Programmer's Network",
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import { GlobalStyles } from 'twin.macro';
 import type { VFC } from 'react';
+// eslint-disable-next-line sort-imports
+import SEO from '../../next-seo.config';
 
 /** アプリケーション ルート コンポーネント */
 const App: VFC<AppProps> = ({ Component, pageProps }) => (
   <>
     <GlobalStyles />
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <DefaultSeo {...SEO} />
     {/* eslint-disable-next-line react/jsx-props-no-spreading */}
     <Component {...pageProps} />
   </>
